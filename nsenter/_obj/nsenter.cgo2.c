80289e353cfff376c702aa4176a826f07302df09
wenziyue <wenzy@weipaitang.com>
2019-03-20 20:23:42 +0800

    feat:

---
+++ nsenter/_obj/nsenter.cgo2.c
@@ -0,0 +1,102 @@
+
+#line 1 "cgo-builtin-prolog"
+#include <stddef.h> /* for ptrdiff_t and size_t below */
+
+/* Define intgo when compiling with GCC.  */
+typedef ptrdiff_t intgo;
+
+typedef struct { const char *p; intgo n; } _GoString_;
+typedef struct { char *p; intgo n; intgo c; } _GoBytes_;
+_GoString_ GoString(char *p);
+_GoString_ GoStringN(char *p, int l);
+_GoBytes_ GoBytes(void *p, int n);
+char *CString(_GoString_);
+void *CBytes(_GoBytes_);
+void *_CMalloc(size_t);
+
+__attribute__ ((unused))
+static size_t _GoStringLen(_GoString_ s) { return s.n; }
+
+__attribute__ ((unused))
+static const char *_GoStringPtr(_GoString_ s) { return s.p; }
+
+#line 3 "/Users/wzy/projects/go/src/github.com/xianlubird/mydocker/nsenter/nsenter.go"
+
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <errno.h>
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+
+__attribute__((constructor)) void enter_namespace(void) {
+	char *mydocker_pid;
+	mydocker_pid = getenv("mydocker_pid");
+	if (mydocker_pid) {
+		//fprintf(stdout, "got mydocker_pid=%s\n", mydocker_pid);
+	} else {
+		//fprintf(stdout, "missing mydocker_pid env skip nsenter");
+		return;
+	}
+	char *mydocker_cmd;
+	mydocker_cmd = getenv("mydocker_cmd");
+	if (mydocker_cmd) {
+		//fprintf(stdout, "got mydocker_cmd=%s\n", mydocker_cmd);
+	} else {
+		//fprintf(stdout, "missing mydocker_cmd env skip nsenter");
+		return;
+	}
+	int i;
+	char nspath[1024];
+	char *namespaces[] = { "ipc", "uts", "net", "pid", "mnt" };
+
+	for (i=0; i<5; i++) {
+		sprintf(nspath, "/proc/%s/ns/%s", mydocker_pid, namespaces[i]);
+		int fd = open(nspath, O_RDONLY);
+
+		if (setns(fd, 0) == -1) {
+			//fprintf(stderr, "setns on %s namespace failed: %s\n", namespaces[i], strerror(errno));
+		} else {
+			//fprintf(stdout, "setns on %s namespace succeeded\n", namespaces[i]);
+		}
+		close(fd);
+	}
+	int res = system(mydocker_cmd);
+	exit(0);
+	return;
+}
+
+#line 1 "cgo-generated-wrapper"
+
+
+#line 1 "cgo-gcc-prolog"
+/*
+  If x and y are not equal, the type will be invalid
+  (have a negative array count) and an inscrutable error will come
+  out of the compiler and hopefully mention "name".
+*/
+#define __cgo_compile_assert_eq(x, y, name) typedef char name[(x-y)*(x-y)*-2+1];
+
+/* Check at compile time that the sizes we use match our expectations. */
+#define __cgo_size_assert(t, n) __cgo_compile_assert_eq(sizeof(t), n, _cgo_sizeof_##t##_is_not_##n)
+
+__cgo_size_assert(char, 1)
+__cgo_size_assert(short, 2)
+__cgo_size_assert(int, 4)
+typedef long long __cgo_long_long;
+__cgo_size_assert(__cgo_long_long, 8)
+__cgo_size_assert(float, 4)
+__cgo_size_assert(double, 8)
+
+extern char* _cgo_topofstack(void);
+
+#include <errno.h>
+#include <string.h>
+
+
+#define CGO_NO_SANITIZE_THREAD
+#define _cgo_tsan_acquire()
+#define _cgo_tsan_release()
+
